7edb7e8afadfa411ee91b82fb15f89b98fda31e9
kekvir <kekvir@ttu.ee>
2021-11-16 14:11:56 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
-import { GridStyleComponent } from './todo-item/grid/grid.component';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { GridStyleComponent } from './todo-item/grid/grid.component';
 
 
 @NgModule({
